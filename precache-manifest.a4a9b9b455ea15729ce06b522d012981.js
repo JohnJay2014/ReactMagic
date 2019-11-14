93f8b5c816b7b1b094e4c27b862d7f34305ea441
sage.zhou@sap.com <sage.zhou@sap.com>
2019-11-14 17:16:47 +0800

    Updates

---
+++ precache-manifest.a4a9b9b455ea15729ce06b522d012981.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1540dc7e1d255107c4ed9638beb177e",
+    "url": "/ReactMagic/static/media/longzhifubao.e1540dc7.jpg"
+  },
+  {
+    "revision": "1d143a018771458904c58dbde7d1011a",
+    "url": "/ReactMagic/static/media/longweixin.1d143a01.jpg"
+  },
+  {
+    "revision": "6f3ef122fbf6cb1c75ea48f662ebeb68",
+    "url": "/ReactMagic/static/media/girl.6f3ef122.jpg"
+  },
+  {
+    "revision": "579ecb409bfd36576ee8",
+    "url": "/ReactMagic/static/js/runtime~main.579ecb40.js"
+  },
+  {
+    "revision": "66fc604ab8cbea368f83",
+    "url": "/ReactMagic/static/js/main.66fc604a.chunk.js"
+  },
+  {
+    "revision": "b549a17c27a4144f15af",
+    "url": "/ReactMagic/static/js/1.b549a17c.chunk.js"
+  },
+  {
+    "revision": "66fc604ab8cbea368f83",
+    "url": "/ReactMagic/static/css/main.aa3a265b.chunk.css"
+  },
+  {
+    "revision": "5932498b5b3732e584a3710434a79c92",
+    "url": "/ReactMagic/index.html"
+  }
+];
